f4405ccc6505615bf23da525a10da0c6ebdeefba
Tony Eng <tony.eng@telus.com>
2023-01-26 09:40:06 -0800

    add admin/categories controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+  end
   get 'about', to: "about#index"
   root to: 'products#index'
 
