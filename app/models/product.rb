5b0816204de86a3ebc9f133b9201d606c811c1c3
Tony Eng <tony.eng@telus.com>
2023-01-25 18:00:29 -0800

    update order details

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  
 
   validates :name, presence: true
   validates :price, presence: true
